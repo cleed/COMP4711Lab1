07541074946b22adc7f3ba8946abfa66e9e7a2fd
cleed.c@hotmail.com <cleed.c@hotmail.com>
2016-01-09 14:47:14 -0800

    Added presentation with html table, along with some styling and comments

--- index.php
+++ index.php
@@ -6,43 +6,32 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
+        <title>Tic Tac Toe</title>
+		<style type="text/css">
+			a{
+				text-decoration: none;
+				color: #000;
+			}
+			.cell{
+				text-align: center;
+			}
+		</style>
     </head>
     <body>
         <?php
-        // put your code here
-//        $name = 'Clyde';
-//        $what = 'geek';
-//        $level = 1;
-//        echo 'Hi, my name is '.$name,', and I am a level '.$level.'
-//        '.$what;
-//        
-//        echo '<br/>';
-//        
-//        $hoursworked = $_GET['hours'];
-//        $rate = 12;
-//        $total = $hoursworked * $rate;
-//        if ($hoursworked > 40) {
-//            $total = $hoursworked * $rate * 1.5;
-//        } else {
-//            $total = $hoursworked * $rate;
-//        }
-//        
-//        echo ($total > 0)?'You owe me '.$total : "You're welcome";
-//        
-        // tictactoe
-        
-        // game class
+
+        // Tic tac toe game
         class Game {
             var $position;
+			// Constructor, saves the gameboard
             function __construct($squares){
                 $this->position = str_split($squares);
             }
             
-            // check winner
+            // Checks for winner
             function winner($token) {
-                // check rows
+                // Check row by row
                 for($row=0; $row<3; $row++) {
                     $result = true;
                     for($col=0; $col<3; $col++){
@@ -51,9 +40,10 @@ and open the template in the editor.
                             break;
                         }
                     }
+					// Returns true if winner found, else move on
                     if($result) return $result;
                 }
-                // check columns
+                // No winner found yet, check column by column
                 for($col=0; $col<3; $col++) {
                     $result = true;
                     for($row=0; $row<3; $row++){
@@ -64,7 +54,7 @@ and open the template in the editor.
                     }
                     if($result) return $result;
                 }
-                // check diagonal
+                // No winner found yet, check diagonals
                 if($this->position[4] == $token){
                     if($this->position[0] == $token && $this->position[8])
                         $result = true;
@@ -76,15 +66,49 @@ and open the template in the editor.
                 return $result;
             }
             
+            // Visually displays the gameboard in an html table format
+			function display() {
+				echo '<table cols=”3” cellspacing="0" width="90" style=”fontsize:large; fontweight:bold; border-collapse=collapse;” border="1">';
+				echo '<tr>'; // open the first row
+				for ($pos=0; $pos<9;$pos++) {
+					echo $this->show_cell($pos);;
+					if ($pos %3 == 2) echo '</tr><tr>'; // start a new row for the next square
+				}
+				echo '</tr>'; // close the last row
+				echo '</table>';
+			}
             
+			// Display the content of a specific position on the board
+			function show_cell($which) {
+				// Loads the token at the current position
+				$token = $this->position[$which];
+				// deal with the easy case
+				if ($token <> '-') return '<td class="cell">'.$token.'</td>';
+				// now the hard case
+				$this->newposition = $this->position; // copy the original
+				$this->newposition[$which] = 'o'; // this would be their move
+				$move = implode($this->newposition); // make a string from the board array
+				$link = '?board='.$move; // this is what we want the link to be
+				// so return a cell containing an anchor and showing a hyphen
+				return '<td class="cell"><a href="'.$link.'">-</a></td>';
+			}
             
-            
+			// AI for picking next best move
+			
+			
             
         
         }
         
-        $squares = $_GET['board'];
+		// Updates the current board upon every request
+		if(isset($_GET['board'])){
+			$squares = $_GET['board'];
+		} else {
+			$squares = "---------";
+		}
         $game = new Game($squares);
+		$game->display($squares);
+		// Stops the game if a winner is found
         if ($game->winner('x')){
             echo 'You win. Lucky guesses!';
         } else if ($game->winner('o')){
@@ -92,7 +116,8 @@ and open the template in the editor.
         } else {
             echo 'No winner yet, but you are losing.';
         }
-        
+		// Restart resets board to empty state
+        echo '<br/><a href="?board=---------">Restart</a>';
         
         
         
